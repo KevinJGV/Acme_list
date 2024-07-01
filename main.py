8580be5ce7db8036097094bc2b2571f091712efa
MarioFelipe19 <felipeparra257@hotmail.com>
2024-07-01 17:36:07 -0500

    Feat: mejora de menu Bienvenida

--- main.py
+++ main.py
@@ -14,13 +14,27 @@ def menu_principal():
     print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╔══════════════════════════════════════╗"))
     print("{:^160}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║            "+ Fore.WHITE +"ACME-list                 "+ Fore.CYAN + "║"))
     print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╠══════════════════════════════════════╣"))
-    print("{:^165}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "║" + Fore.YELLOW + " 1. " + Fore.WHITE + "Opción Uno                        " + Fore.CYAN + "║"))
-    print("{:^165}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "║" + Fore.YELLOW + " 2. " + Fore.WHITE + "Opción Dos                        " + Fore.CYAN + "║"))
+    print("{:^165}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║" + Fore.YELLOW + " 1. " + Fore.WHITE + "Iniciar Sesion                    " + Fore.CYAN + "║"))
+    print("{:^165}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║" + Fore.YELLOW + " 2. " + Fore.WHITE + "Registrarse                       " + Fore.CYAN + "║"))
     print("{:^165}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "║" + Fore.YELLOW + " 3. " + Fore.WHITE + "Salir                             " + Fore.CYAN + "║"))
     print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╚══════════════════════════════════════╝"))
     
+def menu_Inicio_Sesion():
+    print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╔══════════════════════════════════════╗"))
+    print("{:^160}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║            "+ Fore.WHITE +"Inicio de Sesion          "+ Fore.CYAN + "║"))
+    print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╠══════════════════════════════════════╣"))
+    print("{:^165}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║" + Fore.YELLOW + " 1. " + Fore.WHITE + "Correo                            " + Fore.CYAN + "║"))
+    print("{:^165}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║" + Fore.YELLOW + " 2. " + Fore.WHITE + "Contraseña                        " + Fore.CYAN + "║"))
+    print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╚══════════════════════════════════════╝"))
 
-
+def menu_Registro():
+    print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╔══════════════════════════════════════╗"))
+    print("{:^160}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║            "+ Fore.WHITE +"Registro          "+ Fore.CYAN + "║"))
+    print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╠══════════════════════════════════════╣"))
+    print("{:^165}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║" + Fore.YELLOW + " 1. " + Fore.WHITE + "Correo                            " + Fore.CYAN + "║"))
+    print("{:^165}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║" + Fore.YELLOW + " 2. " + Fore.WHITE + "Contraseña                        " + Fore.CYAN + "║"))
+    print("{:^165}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║" + Fore.YELLOW + " 3. " + Fore.WHITE + "Nombre                            " + Fore.CYAN + "║"))
+    print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╚══════════════════════════════════════╝"))
 
 def pedir_opcion():
     try:
@@ -28,9 +42,7 @@ def pedir_opcion():
         print("{:^160}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║"+ Fore.WHITE +"          Ingrese su opcion:          "+ Fore.CYAN +"║"))
         op = int(input("                                                                  "))
         print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╚══════════════════════════════════════╝"))
-        # print(colored('//', 'red')*20)
-        # op = int(input("  Ingrese su opcion: "))
-        # print(colored('//', 'red')*20)
+    
         
         return op
     except Exception:
@@ -38,6 +50,9 @@ def pedir_opcion():
         print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╔══════════════════════════════════════╗"))
         print("{:^160}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║"+ Fore.WHITE +"           OPCION NO VALIDA!          "+ Fore.CYAN +"║"))
         print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╚══════════════════════════════════════╝"))
+
+
+
 
 while True:
     menu_principal()
@@ -51,9 +66,28 @@ while True:
         break
     if op == 1:
         while True:
-            
-            op1 = pedir_opcion()
-            if op1 == 1:
-                print("--"*20)
-                print("hola1")
-                print("--"*20)
+            menu_Inicio_Sesion()
+            print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╔══════════════════════════════════════╗"))
+            print("{:^160}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║"+ Fore.WHITE +"          Ingrese su opcion:          "+ Fore.CYAN +"║"))
+            op = int(input("                                                    Correo:"))
+            print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╚══════════════════════════════════════╝"))
+            print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╔══════════════════════════════════════╗"))
+            print("{:^160}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║"+ Fore.WHITE +"          Ingrese su opcion:          "+ Fore.CYAN +"║"))
+            op = int(input("                                                    Contraseña:"))
+            print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╚══════════════════════════════════════╝"))
+    if op == 2:
+        while True:
+            menu_Registro()
+            print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╔══════════════════════════════════════╗"))
+            print("{:^160}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║"+ Fore.WHITE +"          Ingrese su opcion:          "+ Fore.CYAN +"║"))
+            op = int(input("                                                    Correo:"))
+            print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╚══════════════════════════════════════╝"))
+            print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╔══════════════════════════════════════╗"))
+            print("{:^160}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║"+ Fore.WHITE +"          Ingrese su opcion:          "+ Fore.CYAN +"║"))
+            op = int(input("                                                    Contraseña:"))
+            print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╚══════════════════════════════════════╝"))
+            print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╔══════════════════════════════════════╗"))
+            print("{:^160}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + " ║"+ Fore.WHITE +"          Ingrese su opcion:          "+ Fore.CYAN +"║"))
+            op = int(input("                                                    Nombre:"))
+            print("{:^150}".format(Back.BLACK + Fore.CYAN + Style.BRIGHT + "╚══════════════════════════════════════╝"))
+                
