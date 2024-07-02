123451d08c89231d8c7fbc133c8c8e57882c39c2
alvaroMartinez13 <alvaro.martinezalcina13@gmail.com>
2024-07-02 04:58:44 -0500

    FEAT: Cruds Tarea | Subtarea

--- cruds.py
+++ cruds.py
@@ -15,7 +15,7 @@ def cargar_datos_json(ruta):
 # al agregar los datos utilizar el mismo contenido del archivo antes de modificar + los cambios que se le alla hecho por medio de na variable
 def guardar_datos_json(datos, ruta):
     datos = list(datos)
-    diccionario = json.dumps(datos,ensure_ascii=False, indent=4)
+    diccionario = json.dumps(datos,ensure_ascii=True, indent=4)
     file = open(ruta,"w")
     file.write(diccionario)
     file.close()
@@ -30,6 +30,7 @@ tarea_cancelado = "data_base/tarea_canceladas.json"
 datos_registro = cargar_datos_json(tarea_registro)
 datos_en_curso = cargar_datos_json(tarea_en_curso)
 datos_terminado = cargar_datos_json(tarea_terminado)
+datos_cancelado = cargar_datos_json(tarea_cancelado)
 
 
 #Crea una nueva tarea
@@ -56,44 +57,108 @@ def crea_tarea(id_usuario, tarea:dict):
         return "Error al crear la tarea."
 
 
+#Búsqueda de la tarea
+def traer_tarea(id_tarea):
+
+    indice_registro = funciones.valida_campos("id_tarea", id_tarea, datos_registro, True)
+    indice_en_curso = funciones.valida_campos("id_tarea", id_tarea, datos_en_curso, True)
+    indice_terminado = funciones.valida_campos("id_tarea", id_tarea, datos_terminado, True)
+    indice_cancelado = funciones.valida_campos("id_tarea", id_tarea, datos_cancelado, True)
+
+    if indice_registro is False and indice_en_curso is False and indice_terminado is False and indice_cancelado is False:
+        return "Tarea no encontrada."
+        
+
+    if indice_registro is not False:
+
+        return indice_registro
+
+        
+    if indice_en_curso is not False:
+
+        return indice_en_curso
+
+
+    if indice_terminado is not False:
+
+        return indice_terminado
+
+
+    if indice_cancelado is not False:
+            
+        return indice_cancelado
+
+
 #Modifica una tarea
 def modifica_tarea(id_tarea, tarea:dict):
     
     try:
 
-        indice_registro = funciones.valida_campos("id_tarea", id_tarea, datos_registro, True)
-        indice_en_curso = funciones.valida_campos("id_tarea", id_tarea, datos_en_curso, True)
-        indice_terminado = funciones.valida_campos("id_tarea", id_tarea, datos_terminado, True)
+        indice = traer_tarea(id_tarea)
 
-        if indice_registro is False and indice_en_curso is False and indice_terminado is False:
-            return "Tarea no encontrada."
+        if indice is False:
+            return "Tarea no existe"
         
-
-        if indice_registro is not False:
-
-            datos_registro[indice_registro] = tarea
+        if indice["estado"] == "por hacer":
+        
+            datos_registro[indice] = tarea
 
             guardar_datos_json(datos_registro, tarea_registro)
 
-        
-        if indice_en_curso is not False:
+        if indice["estado"] == "en curso":
 
-            datos_en_curso[indice_en_curso] = tarea
+            datos_en_curso[indice] = tarea
 
             guardar_datos_json(datos_en_curso, tarea_en_curso)
 
+        if indice["estado"] == "terminado":
 
-        if indice_terminado is not False:
-
-            datos_terminado[indice_terminado] = tarea
+            datos_terminado[indice] = tarea
 
             guardar_datos_json(datos_terminado, tarea_terminado)
+
+        if indice["estado"] == "cancelado":
+
+            datos_cancelado[indice] = tarea
+
+            guardar_datos_json(datos_cancelado, tarea_cancelado)
+            
 
         return "Tarea modificada con éxito."
 
     except Exception as e:
         print(f"Error al modificar la tarea: {e}")
         return "Error al modificar la tarea."
+
+
+#Lista de Tareas por hacer
+def lista_tareas_registro():
+    return datos_registro
+
+
+#Lista de Tareas en curso
+def lista_tareas_en_curso():
+    return datos_en_curso
+
+
+#Lista de Tareas terminadas
+def lista_tareas_terminado():
+    return datos_terminado
+
+
+#Lista de Tareas canceladas
+def lista_tareas_cancelado():
+    return datos_cancelado
+
+
+#Lista de Tareas
+def lista_tareas():
+    lista_combinada = []
+    lista_combinada.extend(lista_tareas_registro())
+    lista_combinada.extend(lista_tareas_en_curso())
+    lista_combinada.extend(lista_tareas_terminado())
+    lista_combinada.extend(lista_tareas_cancelado())
+    return lista_combinada
 
 
 #Crea una nueva subtarea
@@ -127,39 +192,78 @@ def crea_subtarea(id_tarea, subtarea:dict):
         return "Error al crear la subtarea."
 
 
+#Trae la subtarea de la tarea
+def trae_subtarea(id_tarea, id_subarea):
+    
+    tarea = traer_tarea(id_tarea)
+
+    if tarea is False:
+        return "No existe la tarea"
+    
+    subtarea = funciones.valida_campos("id_subtarea", id_subarea, tarea["subtarea"], True)
+
+    if subtarea is False:
+        return "No existe la subtarea"
+    
+    return subtarea
+
+
+#Trae todas las subtareas de la Tarea
+def lista_subtareas(id_tarea):
+
+    tarea = traer_tarea(id_tarea)
+
+    if tarea is False:
+        return "No existe la tarea"
+    
+    if tarea["subtarea"]:
+        for subtarea in tarea["subtarea"]:
+            print(subtarea)
+    else:
+        print("La tarea no tiene subtareas")
+    
+
 #Modifica una subtarea
 def modifica_tarea(id_tarea, id_subtarea, subtarea:dict):
+
     try:
-        
-        indice_tarea_registro = funciones.valida_campos("id_tarea", id_tarea, datos_registro, True)
-        indice_tarea_en_curso = funciones.valida_campos("id_tarea", id_tarea, datos_en_curso, True)
-        indice_tarea_terminado = funciones.valida_campos("id_tarea", id_tarea, datos_terminado, True)
 
-        if indice_tarea_registro is False and indice_tarea_en_curso is False and indice_tarea_terminado is False:
-            return "Tarea no encontrada."
+        indice_tarea = traer_tarea(id_tarea)
 
-        if indice_tarea_registro is not False:
-            indice_subtarea = funciones.valida_campos("id_subtarea", id_subtarea, datos_registro[indice_tarea_registro]["subtareas"], True)
+        if indice_tarea is False:
+            return "No existe Tarea" 
 
-            datos_registro[indice_tarea_registro]["subtareas"][indice_subtarea] = subtarea
+        indice_subtarea = trae_subtarea(id_tarea, id_subtarea)
+
+        if indice_subtarea is False:
+            return "No existe la subtarea"
+
+
+        if indice_tarea["estado"] == "por hacer":
+
+            datos_registro[indice_tarea]["subtareas"][indice_subtarea] = subtarea
 
             guardar_datos_json(datos_registro, tarea_registro)
 
-        if indice_tarea_en_curso is not False:
-            indice_subtarea = funciones.valida_campos("id_subtarea", id_subtarea, datos_registro[indice_tarea_en_curso]["subtareas"], True)
 
-            datos_en_curso[indice_tarea_en_curso]["subtareas"][indice_subtarea] = subtarea
+        if indice_tarea["estado"] == "en curso":
+
+            datos_en_curso[indice_tarea]["subtareas"][indice_subtarea] = subtarea
 
             guardar_datos_json(datos_en_curso, tarea_en_curso)
 
+        if indice_tarea["estado"] == "terminado":
 
-        if indice_tarea_terminado is False:
-            indice_subtarea = funciones.valida_campos("id_subtarea", id_subtarea, datos_registro[indice_tarea_terminado]["subtareas"], True)
-
-            datos_terminado[indice_tarea_terminado]["subtareas"][indice_subtarea] = subtarea
+            datos_terminado[indice_tarea]["subtareas"][indice_subtarea] = subtarea
 
             guardar_datos_json(datos_terminado, tarea_terminado)
-        
+
+        if indice_tarea["estado"] == "cancelado":
+
+            datos_cancelado[indice_tarea]["subtareas"][indice_subtarea] = subtarea
+
+            guardar_datos_json(datos_cancelado, tarea_cancelado)
+
         return "Subtarea modificada con éxito."
 
     except Exception as e:
@@ -167,72 +271,110 @@ def modifica_tarea(id_tarea, id_subtarea, subtarea:dict):
         return "Error al modificar la subtarea."
 
 
-#Cambia el estado de la tarea / subtarea
-def estado(id, estado, tarea=True):
+#Cambia el estado de la tarea | subtarea
+def estado(id_tarea, estado, tarea = True, id_subtarea = 0):
     try:
+
+        indice_tarea = traer_tarea(id_tarea)
+
+        if indice_tarea is False:
+            return "No existe tarea"
+        
         if tarea:
-        # Buscar la tarea 
-            indice_tarea_registro = funciones.valida_campos("id_tarea", id, datos_registro, True)
-            indice_tarea_en_curso = funciones.valida_campos("id_tarea", id, datos_en_curso, True)
-            indice_tarea_terminado = funciones.valida_campos("id_tarea", id, datos_terminado, True)
-            
 
-            if indice_tarea_registro is False and indice_tarea_en_curso is False and indice_tarea_terminado is False:
-                return "Tarea no encontrada."
+            if tarea["estado"] == estado:
+                return "No existe cambios"
             
-            if indice_tarea_registro is not False:
-                datos_registro[indice_tarea_registro]["estado"] = estado
+            if  estado == "por hacer":
+
+                id_nuevo = funciones.generador_id(datos_registro)
+                        
+                indice_tarea["id_tarea"] = id_nuevo
+                indice_tarea["estado"] = estado
+
+                del datos_registro[indice_tarea]
+
+                datos_registro.append(indice_tarea)
                 guardar_datos_json(datos_registro, tarea_registro)
 
-            if indice_tarea_en_curso is not False:
-                datos_en_curso[indice_tarea_en_curso]["estado"] = estado
+            if  estado == "en curso":
+
+                id_nuevo = funciones.generador_id(datos_en_curso)
+                        
+                indice_tarea["id_tarea"] = id_nuevo
+                indice_tarea["estado"] = estado
+
+                del datos_en_curso[indice_tarea]
+
+                datos_en_curso.append(indice_tarea)
                 guardar_datos_json(datos_en_curso, tarea_en_curso)
 
-            if indice_tarea_terminado is not False:
-                datos_terminado[indice_tarea_terminado]["estado"] = estado
+            if  estado == "terminado":
+
+                id_nuevo = funciones.generador_id(datos_terminado)
+                        
+                indice_tarea["id_tarea"] = id_nuevo
+                indice_tarea["estado"] = estado
+
+                del datos_terminado[indice_tarea]
+
+                datos_terminado.append(indice_tarea)
                 guardar_datos_json(datos_terminado, tarea_terminado)
-            
-            return "Estado de la tarea modificado con éxito."
 
+            if  estado == "cancelado":
 
-            indice_subtarea = funciones.valida_campos("id_subtarea", id, datos_registro[indice_tarea]["subtareas"], True)
+                id_nuevo = funciones.generador_id(datos_cancelado)
+                        
+                indice_tarea["id_tarea"] = id_nuevo
+                indice_tarea["estado"] = estado
+
+                del datos_cancelado[indice_tarea]
+
+                datos_cancelado.append(indice_tarea)
+                guardar_datos_json(datos_cancelado, tarea_cancelado)
+
+            return "Estado de Tarea exitoso"
+
+        elif tarea is False and id_subtarea > 0:
+
+            indice_subtarea = trae_subtarea(id_tarea, id_subtarea)
+
             if indice_subtarea is False:
-                return "Subtarea no encontrada."
-
-            # Actualizar el estado de la subtarea
-            datos_registro[indice_tarea]["subtareas"][indice_subtarea]["estado"] = estado
-            return "Estado de la subtarea modificado con éxito."
-
-        else:
-            # Buscar la subtarea
-            indice_tarea_registro = funciones.valida_campos("id_tarea", id, datos_registro, True)
-            indice_tarea_en_curso = funciones.valida_campos("id_tarea", id, datos_en_curso, True)
-            indice_tarea_terminado = funciones.valida_campos("id_tarea", id, datos_terminado, True)
+                return "No existe la subtarea"
             
+            if indice_subtarea["estado"] == estado:
+                return "No existen cambios"
+            else:
 
-            if indice_tarea_registro is False and indice_tarea_en_curso is False and indice_tarea_terminado is False:
-                return "Tarea no encontrada."
-            
-            if indice_tarea_registro is not False:
-                datos_registro[indice_tarea_registro]["estado"] = estado
+                if indice_tarea["estado"] == "por hacer":
 
+                    datos_registro[indice_tarea]["subtareas"][indice_subtarea]["estado"] = estado
+
+                    guardar_datos_json(datos_registro, tarea_registro)
+
+                if indice_tarea["estado"] == "en curso":
+
+                    datos_en_curso[indice_tarea]["subtareas"][indice_subtarea]["estado"] = estado
+
+                    guardar_datos_json(datos_en_curso, tarea_en_curso)
                 
-                guardar_datos_json(datos_registro, tarea_registro)
+                if indice_tarea["estado"] == "terminado":
 
-            if indice_tarea_en_curso is not False:
-                datos_en_curso[indice_tarea_en_curso]["estado"] = estado
-                guardar_datos_json(datos_en_curso, tarea_en_curso)
+                    datos_terminado[indice_tarea]["subtareas"][indice_subtarea]["estado"] = estado
 
-            if indice_tarea_terminado is not False:
-                datos_terminado[indice_tarea_terminado]["estado"] = estado
-                guardar_datos_json(datos_terminado, tarea_terminado)
-            
-            return "Estado de la tarea modificado con éxito."
+                    guardar_datos_json(datos_terminado, tarea_terminado)
 
-            # Actualizar el estado de la tarea
-            datos_registro[indice_tarea]["estado"] = estado
-            return "Estado de la tarea modificado con éxito."
+                if indice_tarea["estado"] == "cancelado":
 
+                    datos_cancelado[indice_tarea]["subtareas"][indice_subtarea]["estado"] = estado
+
+                    guardar_datos_json(datos_cancelado, tarea_cancelado)
+
+                return "Estado de Subtarea exitoso"
+        
+        else:
+            return "Se ha generado un Error"
+           
     except Exception as e:
         print(f"Error al modificar el estado: {e}")
         return "Error al modificar el estado."
